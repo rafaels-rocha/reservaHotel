7091e0c35407c028bc6f03daf58c668b9633335e
Rafael Rocha <r.rocha988@hotmail.com>
2024-07-15 20:22:13 -0300

    Mudando arquivo para o xampp

---
+++ hotel_test/sistema/index.php
@@ -0,0 +1,74 @@
+<!-- <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Página incial</title>
+</head>
+<script src="https://cdn.tailwindcss.com"></script>
+
+<body class="">
+  <div class="bg-gray-200 dark:bg-gray-900 w-full border-b p-2 border-gray-200 dark:border-gray-600">
+    <nav>
+        <div class="flex flex-col md:flex-row  rounded-lg  bg-white max-w-screen-xl flex-wrap items-center justify-between mx-auto p-6">
+            <a href="#" class=" flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="../../api/img/images.jpeg" class="h-8" alt="">
+                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Reservar</span>
+            </a>
+            <div class="text-center m-5">
+              <h2>Completamente desenvolvido por Rafael Rocha</h2>
+          </div>
+            <div class="flex md:order-2 space-x-3 mt-2">
+                <a href="../login/login.html">
+                    <button type="button" class="text-white bg-cyan-500	 hover:bg-gray-300 hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 shadow-md text-center">Entrar</button>
+                </a>
+            </div>
+        </div>
+    </nav>
+</div> -->
+<?php
+require_once('../../api/menu_inicial.php')
+?>
+
+<div class="mx-auto mt-2 w-4/5 md:w-3/4 drop-shadow-md bg-gray-200 rounded-md" id="navbar-sticky">
+  <div class="flex flex-col md:flex-row flex-wrap py-4 w-full drop-shadow-sm items-center justify-center">
+    <div class="bg-gray-300 rounded-lg p-4 w-3/5 md:w-1/5 mx-2 mt-2 md:mt-0 flex flex-col justify-center ">
+        <div class="">
+            <span class="block font-bold">Check-in</span>
+            <span class="block text-gray-500">Insira a data</span>
+        </div>
+        <input type="date" class="mt-2 rounded-lg w-full text-center">
+    </div>
+    
+    <div class="bg-gray-300 rounded-lg p-4 w-3/5 md:w-1/5 mx-2 mt-2 md:mt-0 flex flex-col justify-center ">
+        <div class="">
+            <span class="block font-bold">Checkout</span>
+            <span class="block text-gray-500">Insira a data</span>
+        </div>
+        <input type="date" class="mt-2 rounded-lg w-full text-center">
+    </div>
+    <div class="bg-gray-300 rounded-lg p-4 w-3/5 md:w-1/5 mx-2 mt-2 md:mt-0 flex flex-col justify-center ">
+        <div class="">
+            <span class="block font-bold">Hospedes</span>
+            <span class="block text-gray-500">Insira a quantidade</span>
+        </div>
+          <select name="quantidade_hospedes" id="quantidade_hospedes" class="mt-2 rounded-lg w-full text-center">
+              <option value=""></option>
+              <option value="0">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+          </select>
+      </div>
+      <button class="bg-gray-300 rounded-full p-2 w-2/4 md:w-1/6 mx-2 flex justify-center items-center mt-2 md:mt-0">
+          <p class="text-center flex items-center">
+              <img src="../../api/img/search-interface-symbol.png" alt="Ícone de busca" class="h-4 mr-2">
+              Buscar
+          </p>
+      </button>
+  </div>
+</div>
+
+
+  
+</body>
+</html>
